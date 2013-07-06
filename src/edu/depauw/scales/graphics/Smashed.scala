7d79039f6874712085975483f51154ff5143f28f
Ben Harsha <benjaminharsha_2015@depauw.edu>
2013-07-06 01:44:31 -0400

    Create Smashed.scala

---
+++ src/edu/depauw/scales/graphics/Smashed.scala
@@ -0,0 +1,19 @@
+package edu.depauw.scales.graphics
+
+case class Smashed(g: Graphic,w:Int,h:Int) extends Graphic {
+  def render(gc : GraphicsContext) {
+    g.render(gc)
+  }
+  
+  def bounds: java.awt.Rectangle = new java.awt.Rectangle(w,h)
+}
+object Smasher {
+  def apply(g: Graphic) = Smashed(g,0,0)
+}
+object VSmasher {
+  def apply(g: Graphic) = Smashed(g,0,g.bounds.height)
+}a
+
+object HSmasher {
+  def apply(g: Graphic) = Smashed(g,g.bounds.width,0)
+}
