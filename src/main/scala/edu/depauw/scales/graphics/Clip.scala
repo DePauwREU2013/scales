fb0ecc6198ddfa7763f20a0e1343698e72e7f48b
Rudra Vishweshwar <rvishweshwar_2015@depauw.edu>
2013-07-16 21:52:01 -0400

    Commented Clip.

--- src/main/scala/edu/depauw/scales/graphics/Clip.scala
+++ src/main/scala/edu/depauw/scales/graphics/Clip.scala
@@ -3,8 +3,20 @@ package edu.depauw.scales.graphics
 import java.awt.{Shape => jShape}
 import java.awt.geom.Area
 
+/** Class to define the data members and member functions for Clip graphics
+ * 
+ * @author RudraVishweshwar
+ * @param clip the shape of the clip
+ * @param g the graphic to be clipped
+ *
+ */
 case class Clip(clip: Graphic, g: Graphic) extends Graphic {
-  
+    
+	/**Renders the clipped graphic
+     * 
+     * @param gc GraphicContext
+     * @return the clipped graphic.
+     */
   def render(gc : GraphicsContext) {
     val oldClip = gc.g2d.getClip()
     gc.g2d.setClip(clip.shape)
@@ -12,6 +24,7 @@ case class Clip(clip: Graphic, g: Graphic) extends Graphic {
     gc.g2d.setClip(oldClip)
   }
   
+
   override lazy val bounds = g.bounds.createIntersection(clip.bounds)
   
   override lazy val shape = {
@@ -19,6 +32,10 @@ case class Clip(clip: Graphic, g: Graphic) extends Graphic {
     area.intersect(new Area(clip.shape))
     area
   }
-  
+    /**Function to name the clipped image
+     * 
+     * @param name String containing the name
+     * @return nil
+     */
   def withName(name: String) = g.withName(name)
 }
