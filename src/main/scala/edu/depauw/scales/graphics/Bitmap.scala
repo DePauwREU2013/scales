86276f8fad15a09728862c6177efdf79eaebfd60
Rudra Vishweshwar <rvishweshwar_2015@depauw.edu>
2013-07-16 21:27:56 -0400

    Comments for Bitmap. Would be great if someone could check it.

--- src/main/scala/edu/depauw/scales/graphics/Bitmap.scala
+++ src/main/scala/edu/depauw/scales/graphics/Bitmap.scala
@@ -7,6 +7,16 @@ import java.awt.image.BufferedImage
 import java.awt.geom.{Rectangle2D => jRect}
 
 // (x,y) is the upper left corner, (w,h) is the size
+
+/** Class defining the data members of Bitmap and its member functions
+ * 
+ * @author RudraVishweshwar
+ * @param fn is function which takes two doube values and returns a color
+ * @param x the upper left x coordinate of the graphic
+ * @param y the upper left y coordinate of the graphic
+ * @param w the width of the graphic
+ * @param h the height of the graphic
+ */
 case class Bitmap(fn : (Double, Double) => Color,
                   x : Double, y : Double, w : Double, h : Double) extends Graphic {
   val gc = GraphicsEnvironment.getLocalGraphicsEnvironment.getDefaultScreenDevice.getDefaultConfiguration
@@ -18,11 +28,22 @@ case class Bitmap(fn : (Double, Double) => Color,
     img.setRGB(i, j, c.getRGB)
   }
   
-  def render(gc : GraphicsContext) {
+  
+ /** function to draw a Bitmap image
+  *  
+  *  @param gc GraphicContext 
+  *  @return the Bitmap
+  * 
+  */
+def render(gc : GraphicsContext) {
     gc.drawImage(img, x, y, w, h)
   }
   
   override lazy val bounds = new jRect.Double(x, y, w, h)
-  
+ /** Function to name the bitmap
+  *  
+  *  @param name String containing the name
+  *  @return nil
+  */
   def withName(name: String) = Nil
 }
