60ed59ad0859de615003d8fcc503cd13cfe006d1
Mervin Fansler <mmfansler@gmail.com>
2013-07-23 12:33:10 -0400

    adds padding support; clean up some documentation

--- src/main/scala/edu/depauw/scales/graphics/Graphic.scala
+++ src/main/scala/edu/depauw/scales/graphics/Graphic.scala
@@ -4,32 +4,42 @@ import java.awt.geom.{AffineTransform, Rectangle2D => jRect}
 import java.awt.{Shape => jShape}
 
 
-/**This class defines the general member functions of any Graphic
+/**
+ * This class defines the general member functions of any Graphic
  * 
- * @author RudraVishweshwar
- *
  */
 trait Graphic {
+  
   /**
+   * All Graphics must implement this method which handles rendering via
+   * the graphics context.
+   * 
    * @param gc GraphicsContext
-   * @return returns the graphic on the panel
+   * @return Unit
    * 
    */
   def render(gc : GraphicsContext)
-  
 
+  /**
+   * All Graphics should specify a bounding box
+   */
   lazy val bounds: jRect = new jRect.Double()
   
+  /**
+   * All Graphics should specify a java.awt.Shape represention
+   */
   lazy val shape: jShape = bounds
-  /**function to name the Graphic
+  
+  /** Function to query the Graphic for all Graphics with a given name
    * 
-   * @param name String containing the name for the graphic
-   * @return nil
+   * @param name String containing the name of the Graphic(s) to be found
+   * @return List[Graphic] 
    * 
    */
   def withName(name: String): List[Graphic]
   
   /* ========= Compositing methods ========= */
+  
   /** Function to put a Graphic over another Graphic
    * 
    * @param g Graphic to be placed Over
@@ -37,6 +47,7 @@ trait Graphic {
    * 
    */
   def over(g : Graphic) : Graphic = Composite(this, g)
+  
   /** Shortcut function for over
    * 
    * @param g Graphic to be placed Over
@@ -52,6 +63,7 @@ trait Graphic {
    * 
    */
   def |(g : Graphic) : Graphic = this over g
+  
   /** Function to put a Graphic beside another Graphic
    * 
    * @param g Graphic to be placed beside
@@ -62,6 +74,7 @@ trait Graphic {
     Composite(this,
       Translate(this.bounds.getX + this.bounds.getWidth - g.bounds.getX, 0, g))
   }
+  
   /** This function is an alias for backward compatibility
    * 
    * @param g Graphic to be placed beside
@@ -69,7 +82,8 @@ trait Graphic {
    * 
    */
   def |||(g: Graphic): Graphic = this beside g
-  /** Shortcut function for beside
+  
+   /** Shortcut function for beside
    *   
    * @param g Graphic to be placed beside
    * @return Graphic with the new composite
@@ -97,7 +111,8 @@ trait Graphic {
     def -^(g: Graphic): Graphic = this above g
   
   /* ========= Alignment methods ========= */
-  /** Function to move a graphic on the panel
+ 
+   /** Function to move a graphic on the panel
    * 
    * @param x the x coordinate of the position to be moved to
    * @param y the y coordinate of the position to be moved to
@@ -107,7 +122,8 @@ trait Graphic {
   def moveTo(x: Double, y: Double): Graphic = {
     Translate(x - this.bounds.getX, y - this.bounds.getY, this)
   }
-  /** Shortcut function for move
+  
+   /** Shortcut function for moveTo
    * 
    * @param x the x coordinate of the position to be moved to
    * @param y the y coordinate of the position to be moved to
@@ -123,6 +139,7 @@ trait Graphic {
    * 
    */
   def topLeft: Graphic = moveTo(0, 0)
+  
   /** Function to move a graphic to the middle left position
    * 
    * @param nil
@@ -130,6 +147,7 @@ trait Graphic {
    * 
    */
   def middleLeft: Graphic = moveTo(0, -this.bounds.getHeight/2)
+  
   /** Function to move a graphic to the bottom left position
    * 
    * @param nil
@@ -137,6 +155,7 @@ trait Graphic {
    * 
    */
   def bottomLeft: Graphic = moveTo(0, -this.bounds.getHeight)
+  
   /** Function to move a graphic to the top center position
    * 
    * @param nil
@@ -144,6 +163,7 @@ trait Graphic {
    * 
    */
   def topCenter: Graphic = moveTo(-this.bounds.getWidth/2, 0)
+  
   /** Function to move a graphic to the middle center position
    * 
    * @param nil
@@ -152,13 +172,15 @@ trait Graphic {
    */
   def middleCenter: Graphic =
     moveTo(-this.bounds.getWidth/2, -this.bounds.getHeight/2)
-  /** Function to move a graphic to the center position
+  
+    /** Function to move a graphic to the center position
    * 
    * @param nil
    * @return Graphic at the center position
    * 
    */
   def center: Graphic = middleCenter
+  
   /** Function to move a graphic to the bottom center position
    * 
    * @param nil
@@ -167,13 +189,15 @@ trait Graphic {
    */
   def bottomCenter: Graphic =
     moveTo(-this.bounds.getWidth/2, -this.bounds.getHeight)
-  /** Function to move a graphic to the top right position
+  
+   /** Function to move a graphic to the top right position
    * 
    * @param nil
    * @return Graphic at the top right position
    * 
    */
   def topRight: Graphic = moveTo(-this.bounds.getWidth, 0)
+ 
   /** Function to move a graphic to the middle right position
    * 
    * @param nil
@@ -182,7 +206,8 @@ trait Graphic {
    */  
   def middleRight: Graphic =
     moveTo(-this.bounds.getWidth, -this.bounds.getHeight/2)
-  /** Function to move a graphic to the bottom right position
+  
+   /** Function to move a graphic to the bottom right position
    * 
    * @param nil
    * @return Graphic at the bottom right position
@@ -192,13 +217,15 @@ trait Graphic {
     moveTo(-this.bounds.getWidth, -this.bounds.getHeight)
     
     //======= Point returning methods ========
-  /** Function to return the upper left coordinates of the graphic
+  
+    /** Function to return the upper left coordinates of the graphic
    * 
    * @param nil
    * @return (Double, Double) the coordinates of the upper left part of the graphic
    * 
    */  
   def ul: (Double,Double) = (this.bounds.getX(), this.bounds.getY())
+  
   /** Function to return the middle left coordinates of the graphic
    * 
    * @param nil
@@ -214,6 +241,7 @@ trait Graphic {
    * 
    */ 
   def bl: (Double,Double) = (this.bounds.getX(), this.bounds.getY()+this.bounds.getBounds().height)
+  
   /** Function to return the top center coordinates of the graphic
    * 
    * @param nil
@@ -221,6 +249,7 @@ trait Graphic {
    * 
    */ 
   def tc: (Double,Double) = (this.bounds.getX()+(this.bounds.getBounds().width/2),this.bounds.getY())
+  
   /** Function to return the middle center coordinates of the graphic
    * 
    * @param nil
@@ -237,6 +266,7 @@ trait Graphic {
    * 
    */     
   def c: (Double,Double) = mc
+  
   /** Function to return the bottom center coordinates of the graphic
    * 
    * @param nil
@@ -244,6 +274,7 @@ trait Graphic {
    * 
    */ 
   def bc: (Double,Double) = (this.bounds.getX()+(this.bounds.getBounds.width/2),this.bounds.getY+this.bounds.getBounds.height)
+  
   /** Function to return the top right coordinates of the graphic
    * 
    * @param nil
@@ -251,6 +282,7 @@ trait Graphic {
    * 
    */ 
   def tr: (Double,Double) = (this.bounds.getX()+this.bounds.getBounds.width,this.bounds.getY())
+  
   /** Function to return the middle right coordinates of the graphic
    * 
    * @param nil
@@ -269,7 +301,6 @@ trait Graphic {
     
   /* ========= Bounds methods ========= */
   
-  
   /** Function takes a shape and returns the same shape with a zero size bounding box
    * 
    * @param nil
@@ -277,6 +308,7 @@ trait Graphic {
    * 
    */ 
   def smash: Graphic = BoundsChanger(this)
+  
   /** Function takes a shape and returns the same shape with a bounding box with width = 0
    * 
    * @param nil
@@ -284,6 +316,7 @@ trait Graphic {
    * 
    */
   def vSmash: Graphic = BoundsChanger(this,0,this.bounds.getHeight)
+  
   /** Function takes a shape and returns the same shape with a bounding box with height = 0
    * 
    * @param nil
@@ -291,6 +324,7 @@ trait Graphic {
    * 
    */
   def hSmash: Graphic = BoundsChanger(this,this.bounds.getWidth,0)
+  
   /** Function to changing the bounding box of the graphic
    *  
    * @param newWidth Int which is the new Width of the bounding box
@@ -304,6 +338,48 @@ trait Graphic {
   def changeBounds(newWidth: Int, newHeight: Int, moveX: Int = 0, moveY: Int = 0): Graphic = {
     BoundsChanger(this, newWidth, newHeight, moveX, moveY)
   }
+  
+  /** Method to add padding around a Graphic
+ * @param top amount to pad above the graphic
+ * @param left amount to pad left of the graphic
+ * @param bottom amount to pad below the graphic
+ * @param right amount to pad right of the graphic
+ * @return Graphic with a padded bounding box
+ */
+  def pad(top: Double, left: Double, bottom: Double, right: Double): Graphic = {
+    BoundsChanger(this, bounds.getWidth + left + right, bounds.getHeight + top + bottom,
+    			  -left, -top)
+  }
+  
+  /**
+   * @param value amount to pad in every direction around a graphic
+   * @return Graphic with uniformly padded bounding box
+   */
+  def pad(value: Double): Graphic = pad(value, value, value, value)
+  
+  /**
+ * @param v amount to pad above and below the graphic
+ * @param h amount to pad left and right of the graphic
+ * @return Graphic with padded bounding box
+ */
+  def pad(v: Double, h: Double): Graphic = pad(v, h, v, h)
+  
+  def padTop(top: Double): Graphic = pad(top, 0, 0, 0)
+  def padLeft(left: Double): Graphic = pad(0, left, 0, 0)
+  def padBottom(bottom: Double): Graphic = pad(0, 0, bottom, 0)
+  def padRight(right: Double): Graphic = pad(0, 0, 0, right)
+  
+  /**
+   * @param value amount to pad left and right of the graphic
+   * @return Graphic equally padded on its left and right sides
+   */
+  def padH(value: Double): Graphic = pad(0, value, 0, value)
+  
+  /**
+   * @param value amount to pad above and below the graphic
+   * @return Graphic equally padded on its top and bottom sides
+   */
+  def padV(value: Double): Graphic = pad(value, 0, value, 0)
   
   /** Function to freeze the graphic and get its pixel based graphic
    * 
