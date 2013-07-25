fd5f37f29e97fccfd6738db5785df2a31e68c4ec
Mervin Fansler <mmfansler@gmail.com>
2013-07-25 15:51:04 -0400

    creates ScalesApp to reduce boilerplate

--- src/test/scala/edu/depauw/scales/graphics/TestGraphics.scala
+++ src/test/scala/edu/depauw/scales/graphics/TestGraphics.scala
@@ -2,27 +2,21 @@ package edu.depauw.scales.graphics
 
 import java.awt.{Graphics, Graphics2D, BasicStroke, RenderingHints, Font => jFont, Color}
 import javax.swing.{JFrame, JPanel, WindowConstants}
+import edu.depauw.scales.ScalesApp
 
-object TestGraphics extends App {
-  val frame = new JFrame("Graphics Test")
-  frame.setSize(600, 400)
+object TestGraphics extends ScalesApp(600, 600, RenderMode.SCALE_TO_FIT, "Graphics Test") {
   
   val g = Line(25, 25, 75, 75)
   val g2 = Fill(Colors.RED, Polygon((10, 10), (22, 80), (25, 25), (22, 10)))
   
   val serifFont = new Font("Serif", FontStyleType.BOLD | FontStyleType.ITALIC)
-  val g3 = Translate(10, 10, Text("Hello World!", serifFont))
+  val g3 = Text("Hello World!", serifFont) -@ (10,2)
   
   def fun(x: Double, y: Double): Color = RGBA(x*y, (1-x)*y, x*(1-y), (x+1)*(y+1)/4)
   val g4 = Bitmap(fun, 10, 10, 80, 80)
   
-  val panel = new GraphicPanel(0, new java.awt.geom.AffineTransform())
-  panel.graphic = g | g2 | g3 | g4
+  val panel = GraphicPanel()
+  panel.graphic = g -& g2 -& g3 -& g4
   
-  val pane = new ScalesPanel(RenderMode.SCALE_TO_FIT)
-  pane.add(panel)
-  frame.add(pane)
-  
-  frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE)
-  frame.setVisible(true)
+  addPanel(panel)
 }
