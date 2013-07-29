499db573418c4d2d05ce3b2b57c371dcc3ad3402
Mervin Fansler <mmfansler@gmail.com>
2013-07-28 23:00:55 -0400

    minor changes to animtest

--- src/test/scala/edu/depauw/scales/AnimTest.scala
+++ src/test/scala/edu/depauw/scales/AnimTest.scala
@@ -5,32 +5,40 @@ import edu.depauw.scales.music._
 import Util._
 
 object AnimDemo extends App {
-  def resourcePath(fileName: String): String = System.getProperty("user.dir") + "/resources/" + fileName
   
-  val test : Step =
-    2 * Anim(bounce, 5) |
-    (C~.5 + G.<.en) * 10 +
-      (5~Anim(bounce, 1) | (.5~C.> + G.en) * 5)~2.5 +
-      C.< |
+  def resourcePath(fileName: String): String =
+    System.getProperty("user.dir") + "/resources/" + fileName
+  
+  // a bunch of stuff to show in parallel and in sequence
+  val test: Step =
+    // magic jellybean jumping along to a fifth repeated 10 times
+    2 * Anim(bounce, 5) | (C~.5 + G.<.en) * 10 +
+    // bean gets hyper as music speeds up
+    (5~Anim(bounce, 1) | (.5~C.> + G.en) * 5)~2.5 +
+    // the bean vanishes when a low C is played
+    C.< |
+    // everything below is running the entire time:
     Anim(grow, 15) |
-    FPS(1, Anim({_ => Translate(10.0, 95.0, Text(new java.util.Date().toString))}, 17)) |
+    // date time readout
+    FPS(1, Anim({_ => Text(new java.util.Date().toString, FontSize(6)) -+ (10, 95) }, 17)) |
+    // DePauw CS REU advisors
     Pict(Image(resourcePath("examplePicture.jpg"), 80, 80, 10, 10), 17) |
+    // pretty sure this inspired Jony Ive's iOS 7 design
     Pict(Bitmap({(x, y) => RGBA(x, Math.min(x, y), y, 0.5)}, 100, 100), 17) |
-    Pict(Outline(Colors.YELLOW, Fill(Colors.YELLOW, Rectangle(90, 90, 5, 5))), 17)
+    // why not add a frame?!
+    Pict(Square(90) -+ (5,5) -~* Colors.YELLOW, 17)
   
   val director = new Director(test, new ScalesPanel(RenderMode.SCALE_TO_FIT))
   director.start()
   
-  def bounce(v : Double) : Graphic =
-    Stroke(0.5,
-      Shear(0.5, 0,
-        Fill(HSV(2 * v, 1.0, 1.0),
-          Circle((5 --> 30)(v), (10 --> 70)(v), 50 + 30 * sin2pi(5 * v))
-        ) -+ (-40,0)
-      )
-    )
+  /* a magical colorful jellybean riding a sin wave */
+  def bounce(v: Double): Graphic =
+	Shear(0.5, 0,
+	  Circle((5 --> 30)(v), (10 --> 70)(v), 50 + 30 * sin2pi(5 * v)) -+ (-40,0)
+	) -* HSV(2 * v, 1.0, 1.0) -~ 0.5
   
-  def grow(v : Double): Graphic = Fill(RGBA(0, 0, 1, 0.5), 
+  /* A growing, rotating blue bowtie polgon thingy */
+  def grow(v: Double): Graphic = Fill(RGBA(0, 0, 1, 0.5), 
     Polygon((0,0),(100,100),(100,0),(0,100)) -% 2*Pi*v -* v centerAt (50,50)
   )
 }
