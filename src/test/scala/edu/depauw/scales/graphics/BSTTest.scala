6d3bc5369616be0f6bcad8339eeae7ab226df699
benharsha <benjaminharsha_2015@depauw.edu>
2013-07-30 10:26:31 -0400

    BST example completed

--- src/test/scala/edu/depauw/scales/graphics/BSTTest.scala
+++ src/test/scala/edu/depauw/scales/graphics/BSTTest.scala
@@ -3,21 +3,21 @@ package edu.depauw.scales.graphics
 import edu.depauw.scales.ScalesApp
 
 trait Tree {
-  def insert(n: Int): Tree = this match{
-    case Empty => new Node(Empty,n,Empty)
-    case t: Node =>
-      if(n<t.v){
-        return new Node(t.l.insert(n),t.v,t.r)
-      } else if(n>t.v){
-        return new Node(t.l,t.v,t.r.insert(n))
-      } else return this
-  }
+  def insert(n: Int): Tree
 }
-object Empty extends Tree
-case class Node(val l: Tree,val v: Int,val r: Tree) extends Tree
+object Empty extends Tree {
+  def insert(n: Int): Tree = new Node(Empty, n, Empty)
+}
+case class Node(val l: Tree,val v: Int,val r: Tree) extends Tree {
+  def insert(n: Int): Tree = if(n<v){
+	  new Node(l.insert(n),v,r)
+      } else if(n>v){
+        new Node(l,v,r.insert(n))
+      } else this
+}
 
 object BSTTest extends ScalesApp{
-  val bst = makeBST(List(2,1,3,4))
+  val bst = makeBST(List(20,10,30,15,25,11,29,5,40,21,16,1,6,39,42))
   def makeBST(nums: List[Int],t: Tree = Empty):Tree = nums match{
     case head::tail => makeBST(tail,t.insert(head))
     case Nil => t
@@ -30,14 +30,14 @@ object BSTTest extends ScalesApp{
       val left = Name("left"+t.v, if(t.l!=Empty) drawTree(t.l) else Blank())
       val right = Name("right"+t.v, if(t.r!=Empty) drawTree(t.r) else Blank())
       val noLines = center.center.padBottom(20) above 
-          (left.padRight(20) beside right).center
+          (left beside Phantom(Square(20)) beside right).center
       val leftLine = t.l match{
         case Empty => Blank()
-        case t:Node => Line(noLines.withName("left" + t.v)(0).tc,center.bc)
+        case Node(l,v,r) => Line(noLines.withName("left" + t.v)(0).tc,center.bc)
       }
       val rightLine = t.r match{
         case Empty => Blank()
-        case t:Node => Line(noLines.withName("right" + t.v)(0).tc,center.bc)
+        case Node(l,v,r) => Line(noLines.withName("right" + t.v)(0).tc,center.bc)
       }
       noLines -& rightLine -& leftLine
   }
