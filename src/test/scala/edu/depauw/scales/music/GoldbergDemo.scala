7b37b289689f24d041844a8d0d37d652191db106
Vivian <vichen@vassar.edu>
2013-07-31 13:58:58 -0400

    Made changes to GoldbergDemo

--- src/test/scala/edu/depauw/scales/music/GoldbergDemo.scala
+++ src/test/scala/edu/depauw/scales/music/GoldbergDemo.scala
@@ -9,7 +9,7 @@ object GoldbergDemo extends App {
  * Auflosungen No.1
  */
   val bar1 = G.<< + D.< + C.< + B.<< + D.< + E.< + F.sh.< + G.<
-  val A1 = Tempo(100, (bar1 | bar1.reverse))
+  val A1 = bar1 | bar1.reverse
   
   
   /*
@@ -17,9 +17,9 @@ object GoldbergDemo extends App {
  */
   
   val bar2 = D + G.< + A.< + B.< + G.< + F.sh.< + E.< + D.<
-  val A2 = Tempo(100, (bar2| bar2.reverse))
+  val A2 = bar2 | bar2.reverse
 
-  (new Director(A2)).start
+  (new Director(bar1.reverse + Rest(4) + bar1 + Rest(4) + A1)).start
   
 
 }
