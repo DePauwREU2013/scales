a43cbdee215573933f5e7e08c4c1760254cceb1e
Mervin Fansler <mmfansler@gmail.com>
2013-08-01 01:03:10 -0400

    added sampled instruments to goldberg demo

--- src/test/scala/edu/depauw/scales/music/GoldbergDemo.scala
+++ src/test/scala/edu/depauw/scales/music/GoldbergDemo.scala
@@ -1,25 +1,37 @@
 package edu.depauw.scales.music
 
 import edu.depauw.scales._
+import java.io.File
+import javax.sound.midi.Patch
 
 
 object GoldbergDemo extends App {
   
   /*
- * Auflosungen No.1
+ * Auflösung No.1
  */
   val bar1 = G.<< + D.< + C.< + B.<< + D.< + E.< + F.sh.< + G.<
   val A1 = bar1 | bar1.reverse
   
   
   /*
- * Auflosungen No.2
+ * Auflösung No.2
  */
   
   val bar2 = D + G.< + A.< + B.< + G.< + F.sh.< + E.< + D.<
   val A2 = bar2 | bar2.reverse
 
-  (new Director(bar1.reverse + Rest(4) + bar1 + Rest(4) + A1)).start
+  val sompopFalsetto = new File(System.getProperty("user.dir") + "/resources/sompop.wav")
   
+  val director = new Director(
+      bar1.reverse + Rest(4) +
+      bar1 + Rest(4) +
+      A1 + Rest(4) +
+      Instrument((0,127), Volume(120,A1))
+    )
+  
+  director.loadInstrument(sompopFalsetto, new Patch(0,127))
+  
+  director.start
 
 }
